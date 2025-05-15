3081673e116f0f352e8990493637d2a649ef10f0
Anurag Bhardwaj <86941924+theanuraaag@users.noreply.github.com>
2025-05-15 20:54:02 +0530

    Added movedZeroes

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/move-zeroes/description/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[index++]=nums[i];
+            }
+        }
+        while(index < nums.length)
+        {
+            nums[index++]=0;
+        }
+
+    }
+}
+
+
+// Time Complexity: O(N)
+// Space Complexity : O(1)
