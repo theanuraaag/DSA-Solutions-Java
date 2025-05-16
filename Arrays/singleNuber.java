d3fb04eac6ab76555ddde209a06c2b5a1dad35de
Anurag Bhardwaj <86941924+theanuraaag@users.noreply.github.com>
2025-05-16 10:23:12 +0530

    Added Single Number Problem

---
+++ Arrays/singleNuber.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/single-number/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result =0;
+        for(int i=0;i<nums.length;i++)
+        {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
