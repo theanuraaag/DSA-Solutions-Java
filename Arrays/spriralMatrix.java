740b8edf880c3a5e39864ad79d364d5ff2196899
Anurag Bhardwaj <86941924+theanuraaag@users.noreply.github.com>
2025-05-25 18:45:43 +0530

    solved spriral matrix using brute force

---
+++ Arrays/spriralMatrix.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/spiral-matrix/description/
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n= matrix[0].length;
+        int startRow = 0;
+        int endRow = m-1;
+        int startCol = 0;
+        int endCol = n-1;
+        List <Integer> list = new ArrayList<>();
+        while(startRow <= endRow && startCol <= startCol)
+        {
+            for(int i=startCol; i <= endCol;i++)
+            {
+                list.add(matrix[startRow][i]);
+            }
+            ++startRow;
+            for(int i=startRow;i <=endRow;i++)
+            {
+                list.add(matrix[i][endCol]);
+            }
+            --endCol;
+           if(startRow <= endRow)
+            {
+                for(int i=endCol;i >= startCol;i--)
+                {
+                    list.add(matrix[endRow][i]);
+                }
+                --endRow;           
+            }
+            if(startCol <= endCol)
+            {
+                for(int i=endRow;i >= startRow;i--)
+                {
+                    list.add(matrix[i][startCol]);
+                }
+                ++startCol;
+            }
+        }
+        return list;
+    }
+}
+
+// Time Complexity : O(N^3)
+// Space Complexity : O(N)
