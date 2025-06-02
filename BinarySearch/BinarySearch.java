f77e8d8fbc30225f80cb6e02ede460d6f557252c
Anurag Bhardwaj <86941924+theanuraaag@users.noreply.github.com>
2025-06-02 22:42:23 +0530

    Binary Search

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/binary-search/description/
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+
+            if (nums[mid]>target){
+                end=mid -1;
+            }
+            else if (nums[mid]<target){
+                start=mid +1;
+            }
+            else
+            {
+                return mid;
+            }
+
+        }
+        return-1;
+    }
+}
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
